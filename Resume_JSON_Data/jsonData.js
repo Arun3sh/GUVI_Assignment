243245f6f882ac240e46c4a67ab85b22547ab9bd
Aruneshwaran <65992799+Arun3sh@users.noreply.github.com>
2021-06-18 20:38:51 +0530

    JavaScript file is added

---
+++ Resume_JSON_Data/jsonData.js
@@ -0,0 +1,7 @@
+var arr = [{Name:"Aruneshwaran", "Email id": "m.aruneshwar@gmail.com", "Phone Number" : 7845796425, "Total Experience": "1.6 Years","PROFESSIONAL SUMMARY ":["1.6 years of experience to work along customer to improve resolve customer queries and enhance work efficiency. "," Exemplary Communication & interpersonal skill with abilities to deftly resolve and troubleshoot issues"] ,"Educational Qualification": "B.E in Automobile", "Soft Skills": ["Problem Solving","Teamwork", "Communication"],"Professional Skills": ["Basic knowledge in C#, JAVASCRIPT, Python and OOPS Concept","Basic knowledge in MS-SQL.","Academic Knowledge in Brain Storming.","Basic knowledge in benchmarking."]}]
+
+for (var i in arr) {
+  for(var j in arr[i]){
+    console.log(j + " : " + arr[i][j]);
+  }
+}
